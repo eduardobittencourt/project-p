f0d01f37693c53400f28a7b77dc124da91a21855
Eduardo Bittencourt <eduardobittencourt08@gmail.com>
2024-03-16 15:24:01 -0300

    test: font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,29 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <header>
-        <h1 className="flex flex-col gap-4 text-center">
-          <span className="font-heading text-2xl text-gray">
+      <header className="flex flex-col gap-8 pb-10">
+        <h1 className="flex flex-col gap-3 text-center">
+          <span className="font-heading text-3xl text-gray">
             Bolão da chegada
           </span>
           <span className="font-calligraphy text-5xl text-brown">Theo</span>
         </h1>
 
-        <div>
+        <div className="flex flex-col gap-8">
           <Image
             src="/axolotl.png"
-            width={500}
-            height={500}
+            width={445}
+            height={319}
             alt="An axolotl"
-            className="mx-auto"
+            className="mx-auto pb-4"
           />
 
-          <p className="mb-8 font-serif text-lg text-gray">
+          <p className="font-serif text-xl text-gray">
             Bem vindo ao Bolão da Chegada do Theo! Escolhemos essa brincadeira
             pra vocês participarem conosco desse momento tão especial.
           </p>
 
-          <p className="mb-8 font-serif text-lg text-gray">
+          <p className="font-serif text-xl text-gray">
             Um pacote de fralda e um pouco de sorte podem te render um voucher
             do iFood!
           </p>
