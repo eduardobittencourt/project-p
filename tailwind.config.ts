d1e4ba17bf74425762aa01f6aed4bcec7ed01ce5
Eduardo Bittencourt <eduardobittencourt08@gmail.com>
2024-03-15 21:49:05 -0300

    feat: playing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      calligraphy: ["var(--font-calligraphy)"],
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      brown: "#af7e55",
+      yellow: "#ebcd94",
+      ambar: "#e4aa74",
+      gray: "#707474",
     },
   },
   plugins: [],
